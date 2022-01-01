9928bcf3f61473e50513bdbe4f5b1c35a1924ae5
Travis Helms <smasher.coder@gmail.com>
2022-01-01 14:25:43 -0600

    user routes change

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,33 +1,44 @@
 const router = require("express").Router();
-// const { User } = require('../../models');
+const { User } = require('../../models');
 
-// GET /api/users
-// router.get('/', (req, res) => {
-//     // Access our User model and run .findAll() method)
-//     User.findAll()
-//       .then(dbUserData => res.json(dbUserData))
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
 
-// // GET /api/users/1
-// router.get('/:id', (req, res) => {});
+        const validPassword = dbUserData.checkPassword(req.body.password);
 
-// POST /api/users
-// router.post('/', (req, res) => {
-//     User.create({
-//       username: req.body.username,
-//       firstname: req.body.firstname,
-//       lastname: req.body.lastname,
-//       password: req.body.password
-//     })
-//       .then(dbUserData => res.json(dbUserData))
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
